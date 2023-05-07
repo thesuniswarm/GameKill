9847656e965ea259c2a047fa6b800d37b281a4f3
theSun <2227405007@qq.com>
2023-05-07 22:40:00 +0800

    planGame

---
+++ src/settings.eeb9b.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/game.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"0df0f",main:"215ea"}};
